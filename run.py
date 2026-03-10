03053cda49c44266b2f711057737eac7dbaec3f6
Saba <yourgithubemail.com>
2026-03-10 22:11:55 +0530

    fix run file

--- run.py
+++ run.py
@@ -2,12 +2,12 @@ from app import create_app, db
 
 app = create_app()
 
+@app.route("/")
+def home():
+    return "Backend running!"
+
 with app.app_context():
     db.create_all()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
-    
-@app.route("/")
-def home():
-    return "backend running!"
+    app.run(host="0.0.0.0", port=5000)
