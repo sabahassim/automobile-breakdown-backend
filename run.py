7440c1ffaf6bc9e25a652e81182fe9406a26b79c
Saba <yourgithubemail.com>
2026-03-12 10:39:04 +0530

    remove db.create_all for production

--- run.py
+++ run.py
@@ -1,13 +1,10 @@
-from app import create_app, db
+from app import create_app
 
 app = create_app()
 
 @app.route("/")
 def home():
     return "Backend running!"
-
-with app.app_context():
-    db.create_all()
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
